ec6255c15566e296251c90488d0aaab6721be676
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2025-08-24 17:07:23 +0600

    done

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], 
+  server: {
+    host: true,   // exposes your local IP
+    port: 5173,   // you can change this if needed
+  },
 })
